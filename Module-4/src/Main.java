3561ac7cbcc8f3f107984f6b42c4777f183d502f
Aditya Deshpande <deshpande.ad@husky.neu.edu>
2020-04-30 15:29:05 -0400

    Completed the first exercise of this module

---
+++ Module-4/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    /**
+     * Main method here calling the pringConversion method of SpeedConverter class to print the output values.
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpeedConverter.printConversion(1.5);
+        SpeedConverter.printConversion(10.25);
+        SpeedConverter.printConversion(-5.6);
+        SpeedConverter.printConversion(25.42);
+        SpeedConverter.printConversion(75.114);
+
+    }
+}
--- Module-4/src/SpeedConverter.java
+++ Module-4/src/SpeedConverter.java
@@ -1,3 +1,35 @@
 public class SpeedConverter {
-    
+
+    /**
+     * This method calculates the speed conversions from kilometersPerHour to milesPerHour.
+     * @param kilometersPerHour
+     * @return
+     */
+    public static long toMilesPerHour(double kilometersPerHour){
+        double converter= 0.621371;
+        if (kilometersPerHour<0){
+            return -1;
+        }
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    /**
+     * This method prints the values of the above converted method.
+     * @param kilometersPerHour
+     */
+    public static void printConversion(double kilometersPerHour){
+      if(kilometersPerHour<0){
+          System.out.println("Invalid value");
+      }
+      else {
+          long milesPerHour = toMilesPerHour(kilometersPerHour);
+          System.out.println(kilometersPerHour +
+                  " km/h= " + milesPerHour +
+                  " mi/h");
+
+      }
+
+    }
+
+
 }
